8ff88782af8c0c9b37cacd566986ab46ace51bfa
dterefe <daveterefe@mail.com>
2025-12-03 13:14:36 +0100

    Helper component.

---
+++ DUUIWeb/src/lib/svelte/components/TooltipTrigger.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { faWarning } from '@fortawesome/free-solid-svg-icons'
+	import Popup from '$lib/svelte/components/Popup.svelte'
+	import Tip from '$lib/svelte/components/Tip.svelte'
+
+	export let tooltipVisible: boolean = false
+	export let tooltipMessage: string = ''
+	export let tipIcon = faWarning
+	export let tipTheme: string = 'tertiary'
+	export let autoPopupWidth: boolean = true
+</script>
+
+{#if tooltipVisible}
+	<Popup {autoPopupWidth}>
+		<svelte:fragment slot="trigger">
+			<slot />
+		</svelte:fragment>
+		<svelte:fragment slot="popup">
+			<Tip customIcon={tipIcon} tipTheme={tipTheme}>{tooltipMessage}</Tip>
+		</svelte:fragment>
+	</Popup>
+{:else}
+	<slot />
+{/if}
